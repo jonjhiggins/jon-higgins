0629d43c201acc2553d5828f41f97d6ff3857a0e
Jon Higgins <jon@jonhiggins.co.uk>
2015-09-30 17:56:34 +0100

    Add work controller

--- src/js/controllers/work.js
+++ src/js/controllers/work.js
@@ -1,8 +1,8 @@
 module.exports = function() {
 	return {
 		show: function() {
-			var home = [1,25,54];
-			return home;
+			var work = [1,25,54];
+			return work;
 		}
 	};
 };
--- src/js/jhApp.js
+++ src/js/jhApp.js
@@ -6,13 +6,51 @@
 
 		var Marionette = require('backbone.marionette'),
 			Backbone = require('backbone'),
-			homeController = require('./controllers/home');
+			homeController = require('./controllers/home'),
+			workController = require('./controllers/work');
 
 	// App
 
 		window.jhApp = new Marionette.Application();
 
+	// Router
+
+		jhApp.Router = Marionette.AppRouter.extend({
+			appRoutes: {
+				'': 'showHome',
+				'work': 'showWork'
+			}
+		});
+
+		var API = {
+			showHome: function() {
+				jhApp.homeController.show();
+			},
+			showWork: function() {
+				jhApp.workController.show();
+			}
+		};
+
 	// Controllers
 
 		jhApp.homeController = homeController();
+		jhApp.workController = workController();
+
+	// Views
+
+		jhApp.on('start', function() {
+
+			new jhApp.Router({
+				controller: API
+			});
+
+			if (Backbone.history) {
+				Backbone.history.start();
+			}
+		});
+
+	// Start
+
+		jhApp.start();
+
 }());
