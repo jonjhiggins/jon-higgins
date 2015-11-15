f06dd09127deaae608e68b87f18247d9fefc8b39
Jon Higgins <jon@jonhiggins.co.uk>
2015-11-15 22:04:37 +0000

    HBS markdown helper in place

--- src/js/work/WorkArticleItemView.js
+++ src/js/work/WorkArticleItemView.js
@@ -1,6 +1,10 @@
 var Marionette = require('backbone.marionette'),
     template = require('./WorkArticleItemTemplate.hbs'),
+    HandlebarsCompiler = require('hbsfy/runtime'),
     WorkArticleItemView;
+
+// Register handlebars markdown helper
+HandlebarsCompiler.registerHelper('md', require('helper-md'));
 
 WorkArticleItemView = Marionette.ItemView.extend({
     template: template,
