1eaaa17dede58f898f0a3bcdf5666faf2c7f55c0
Jon Higgins <jon@jonhiggins.co.uk>
2016-02-04 21:15:32 +0000

    hero image in place on mobile

--- dist/assets/js/bundle.js
+++ dist/assets/js/bundle.js
@@ -42213,9 +42213,9 @@ module.exports = HandlebarsCompiler.template({"1":function(container,depth0,help
 },"2":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return "                <figure class=\"article__hero-image\">\n                    <img src=\"/assets/img/"
+  return "                <figure class=\"article__hero-image\">\n                    <span class=\"article__hero-image-holder\"><img src=\"/assets/img/"
     + container.escapeExpression(container.lambda((depth0 != null ? depth0.image : depth0), depth0))
-    + "\" alt=\"\" />\n                    "
+    + "\" alt=\"\" /></span>\n                    "
     + ((stack1 = helpers["if"].call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.caption : depth0),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "\n                </figure>\n";
 },"3":function(container,depth0,helpers,partials,data) {
