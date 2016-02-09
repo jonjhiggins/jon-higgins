3e8d75508be6ff2f9ca585c678f94b7ea8876743
Jon Higgins <jon@jonhiggins.co.uk>
2016-02-09 11:11:29 +0000

    articleitemview animation - outline

--- dist/assets/js/bundle.js
+++ dist/assets/js/bundle.js
@@ -41752,8 +41752,6 @@ var RegionHideShow = function() {
 
         showAnimated: function(view, options) {
 
-            /*globals console*/ console.log('showAnimated');
-
             // If hiding, defer until finished
             if (this.hidingDeferredRequired) {
 
@@ -41779,7 +41777,6 @@ var RegionHideShow = function() {
         },
 
         hideAnimated: function() {
-            /*globals console*/ console.log('hideAnimated');
 
             // Reset deferred
             this.hidingDeferred = $.Deferred();
@@ -42282,11 +42279,15 @@ module.exports = HandlebarsCompiler.template({"1":function(container,depth0,help
 },"useData":true});
 
 },{"hbsfy/runtime":28}],127:[function(require,module,exports){
+/*globals Bounce, Back, Elastic, Power2*/
+
 var Marionette = require('backbone.marionette'),
     templateSingle = require('./ArticleTemplate.hbs'),
     templateCollection = require('./ArticleItemTemplate.hbs'),
     HandlebarsCompiler = require('hbsfy/runtime'),
     moment = require('moment'),
+    TweenMax = require('gsap/src/uncompressed/TweenMax.js'),
+    $ = require('jQuery'),
     ArticleItemView;
 
 // Register handlebars markdown helper
@@ -42306,6 +42307,75 @@ ArticleItemView = Marionette.ItemView.extend({
         }
 
     },
+
+    buildOutline: function(width, height, top, left, borderColor) {
+
+        var $outline = $('<div class="outline"><span></span><span></span><span></span><span></span></div>');
+
+
+        $outline.css({
+            width: width,
+            height: height,
+            top: top,
+            left: left,
+            color: borderColor
+        });
+
+        $outline.find('span').each(function(index, item) {
+            var $item = $(item);
+
+            if (index === 0) {
+                TweenMax.fromTo($item, 0.4, {scaleY: 0, transformOrigin: '50% 100%'}, {scaleY: 1, delay: 0.4});
+            } else if (index === 1) {
+                TweenMax.fromTo($item, 0.4, {scaleX: 0, transformOrigin: '0% 50%'}, {scaleX: 1, delay: 0.8});
+            } else if (index === 2) {
+                TweenMax.fromTo($item, 0.4, {scaleY: 0, transformOrigin: '50% 0%'}, {scaleY: 1, delay: 1.2});
+            } else if (index === 3) {
+                TweenMax.fromTo($item, 0.4, {scaleX: 0, transformOrigin: '100% 50%'}, {scaleX: 1, delay: 1.6});
+            }
+
+        });
+
+        return $outline;
+    },
+
+    onShow: function() {
+
+        // Selectors
+        var $header = this.$el.find('.article-item__header'),
+            $footer = this.$el.find('.article-item__footer'),
+            $headerFooter = this.$el.find('.article-item__header, .article-item__footer'),
+            $image = this.$el.find('.article-item__image');
+
+        // Create outline
+        var outlineWidth = $header.outerWidth(),
+            outlineHeight = $header.outerHeight() + $footer.outerHeight(),
+            outlineTop = $header.position().top,
+            outlineLeft = $header.position().left,
+            outlineBorderColor = $header.css('background-color'),
+            $outline = this.buildOutline(outlineWidth, outlineHeight, outlineTop, outlineLeft, outlineBorderColor);
+
+        this.$el.append($outline);
+        TweenMax.from($headerFooter.find('*'), 0.4, {opacity: '0', delay: 2}, Power2.ease);
+        TweenMax.from($headerFooter, 0.4, {backgroundColor: 'rgba(255,255,255,0)', delay: 2}, Power2.ease);
+
+        // Create image outline
+        if ($image.length) {
+            var outlineImageWidth = $image.outerWidth(),
+                outlineImageHeight = $image.outerHeight(),
+                outlineImageTop = $image.position().top,
+                outlineImageLeft = $image.position().left,
+                outlineImageBorderColor = $image.css('color'),
+                $outlineImage = this.buildOutline(outlineImageWidth, outlineImageHeight, outlineImageTop, outlineImageLeft, outlineImageBorderColor);
+
+            this.$el.append($outlineImage);
+            TweenMax.from($image, 0.4, {backgroundColor: 'rgba(255,255,255,0)', delay: 2}, Power2.ease);
+            TweenMax.from($image.find('*'), 0.4, {opacity: '0', delay: 2}, Power2.ease);
+        }
+
+        TweenMax.to(this.$el.find('.outline'), 0.4, {autoAlpha: 0, delay: 2}, Power2.ease);
+
+    },
     templateHelpers: function() {
         return {
 
@@ -42323,7 +42393,7 @@ ArticleItemView = Marionette.ItemView.extend({
 
 module.exports = ArticleItemView;
 
-},{"./ArticleItemTemplate.hbs":126,"./ArticleTemplate.hbs":128,"backbone.marionette":4,"hbsfy/runtime":28,"helper-md":29,"moment":35}],128:[function(require,module,exports){
+},{"./ArticleItemTemplate.hbs":126,"./ArticleTemplate.hbs":128,"backbone.marionette":4,"gsap/src/uncompressed/TweenMax.js":8,"hbsfy/runtime":28,"helper-md":29,"jQuery":31,"moment":35}],128:[function(require,module,exports){
 // hbsfy compiled Handlebars template
 var HandlebarsCompiler = require('hbsfy/runtime');
 module.exports = HandlebarsCompiler.template({"1":function(container,depth0,helpers,partials,data) {
